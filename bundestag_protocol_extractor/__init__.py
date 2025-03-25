0f85a6928ccc1f88ccdf69dcdf777e05c70a31f0
Max Boettinger <max@bttngr.de>
2025-03-25 15:21:57 +0100

    Bump version to 0.3.1

--- bundestag_protocol_extractor/__init__.py
+++ bundestag_protocol_extractor/__init__.py
@@ -5,7 +5,7 @@ A package that extracts and structures information from the German Bundestag's o
 particularly plenarprotocols (parliamentary sessions) with full-text speeches.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Max Boettinger"
 __email__ = "github@bttngr.de"
 
